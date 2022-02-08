d79036be1cf9aab0e837a65e7f0eb6760672d3c3
RiedleroD <riedler@gelse.eu>
2022-02-08 11:36:49 +0100

    hopefully fixed texture drawing… implemented custom function to do the deed

--- func.ino
+++ func.ino
@@ -15,7 +15,7 @@ void checkEEPROM() {
 
 void showLogo() { // show logo with (bad) fade-in and -out
 	display.dim(true);
-	display.drawGrayscaleBitmap(0, 0, logo, 128, 64);
+	drawTexture(0, 0, logo, 128, 64);
 	display.display();
 	_delay_ms(250);
 	display.dim(false);
@@ -24,6 +24,46 @@ void showLogo() { // show logo with (bad) fade-in and -out
 	_delay_ms(250);
 	display.clearDisplay();
 	display.display();
+}
+
+/*!
+ * Draws a texture to screen, where one bit equals one pixel
+ * no boundary checking is being done, keep this in mind!
+ * the texture goes from left to right, top to bottom.
+ * @param	_x		left boundary on the x-axis
+ * @param	_y		top boundary on the y-axis
+ * @param	texture	texture to be drawn
+ * @param	width	width of the texture
+ * @param	height	height of the texture
+ * TODO: parameter to draw white as transparent
+ */
+void drawTexture(uint8_t _x,uint8_t _y,const uint8_t texture[],uint8_t width,uint8_t height){
+	/* _y is the first y-coordinate
+	 * y_ is the last y-coordinate
+	 * y is the current y-coordinate
+	 * same for x
+	 */
+	const uint8_t y_=_y+height;
+	const uint8_t x_=_x+width;
+	uint8_t i=0;//index of current uint8_t
+	uint8_t j=0;//index in current uint8_t
+	display.startWrite();
+	for(uint8_t y=_y;y<y_;++y){
+		for(uint8_t x=_x;x<x_;++x){
+			//reading single bit from texture array;
+			//pretty sure the lib knows that we're working with a single-bit
+			//color & we don't need values larger than 1 to draw pixels
+			display.writePixel(x,y,( 1 & (texture[i] >> j) ));
+		}
+		//incrementing counters
+		if(j>=15){
+			j=0;
+			++i;
+		}else{
+			++j;
+		}
+	}	
+	display.endWrite();
 }
 
 /*void drawMenu() {
