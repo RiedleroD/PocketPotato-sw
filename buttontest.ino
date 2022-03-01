feac6d681d1ea22eb38501ef83beb9e343fbd8c5
RiedleroD <riedler@gelse.eu>
2022-03-01 13:05:43 +0100

    formatting

--- buttontest.ino
+++ buttontest.ino
@@ -3,21 +3,21 @@
  */
 
 void testButtons() {
-  while (true) {
-    if (down.isClick() || down.isStep())
-      display.print("DOWN");
-    else if (up.isClick() || up.isStep())
-      display.print("UP");
-    else if (left.isClick()) {
-      display.print("LEFT");
-    }
-    else if (right.isClick() || sh_r.isClick()) {
-      display.print("RIGHT");
-    }
-    display.display();
-
-    if(up.isHold() && down.isHold()) {
-      return;
-    }
-  }
+	while (true) {
+		if (down.isClick() || down.isStep())
+			display.print("DOWN");
+		else if (up.isClick() || up.isStep())
+			display.print("UP");
+		else if (left.isClick()) {
+			display.print("LEFT");
+		}
+		else if (right.isClick() || sh_r.isClick()) {
+			display.print("RIGHT");
+		}
+		display.display();
+		
+		if(up.isHold() && down.isHold()) {
+			return;
+		}
+	}
 }
--- testgame.ino
+++ testgame.ino
@@ -1,31 +1,31 @@
 void gameTest() {
-  uint8_t x = 0;
-  uint8_t y = 0;
-  unsigned long nextTime = 0;
+	uint8_t x = 0;
+	uint8_t y = 0;
+	unsigned long nextTime = 0;
 
-  while (true) {
-    if(left.isClick() || left.isStep())
-      x = constrain(x - 1, 0, 127);
-    if(right.isClick() || right.isStep())
-      x = constrain(x + 1, 0, 127);
-    if(up.isClick() || up.isStep())
-      y = constrain(y - 1, 0, 63);
-    if(down.isClick() || down.isStep())
-      y = constrain(y + 1, 0, 63);
+	while (true) {
+		if(left.isClick() || left.isStep())
+			x = constrain(x - 1, 0, 127);
+		if(right.isClick() || right.isStep())
+			x = constrain(x + 1, 0, 127);
+		if(up.isClick() || up.isStep())
+			y = constrain(y - 1, 0, 63);
+		if(down.isClick() || down.isStep())
+			y = constrain(y + 1, 0, 63);
 
 
-    if(millis() > nextTime) {
-      Serial.print(x);
-      Serial.print("/");
-      Serial.println(y);
-      nextTime = millis() + 1000;
-    }
-    
-    display.clearDisplay();
-    drawTexture(x, y, smile, 6, 4);
+		if(millis() > nextTime) {
+			Serial.print(x);
+			Serial.print("/");
+			Serial.println(y);
+			nextTime = millis() + 1000;
+		}
+		
+		display.clearDisplay();
+		drawTexture(x, y, smile, 6, 4);
 
-    display.display();
-    if(up.isHold() && down.isHold())
-      return;
-  }
+		display.display();
+		if(up.isHold() && down.isHold())
+			return;
+	}
 }
