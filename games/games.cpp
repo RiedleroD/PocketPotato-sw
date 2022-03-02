24cbd22b78c80f13702b646483f5f179c3960879
RiedleroD <riedler@gelse.eu>
2022-03-02 09:35:35 +0100

    moved all the games into a separate folder

---
+++ games/games.cpp
@@ -0,0 +1,7 @@
+//defining functions for the games
+extern void drawTexture(const uint8_t _x,const uint8_t _y,const uint8_t texture[],const uint8_t width,const uint8_t height);
+
+#include "Snake.cpp"
+#include "buttontest.cpp"
+#include "testgame.cpp"
+#include "eepromtest.cpp"
