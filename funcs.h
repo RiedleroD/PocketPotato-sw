a28c9eb436c4033e2346174d243c8dcd94714e6f
RiedleroD <riedler@gelse.eu>
2022-03-22 14:44:06 +0100

    removed printFromFlash header

--- funcs.h
+++ funcs.h
@@ -45,4 +45,3 @@
 
 extern void drawTexture(const uint8_t _x,const uint8_t _y,const uint8_t texture[],const uint8_t width,const uint8_t height);
 extern void drawZoomedPixel(const uint8_t x,const uint8_t y,uint8_t zoom);
-extern void printFromFlash(char* text);
